0f5063e51cc1c833355b9417a37522c0ab4c67c0
gibifyofficial <evandrogibicoski@outlook.com>
2021-03-21 15:26:19 -0300

    Refactoring App
    Refactored

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,46 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect} from 'react';
+
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+interface MoviesProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContenProps {
+  movies: MoviesProps[];
+  title: string;
+}
+
+export function Content({ movies, title}: ContenProps) {
+  return (
+    <div className="container">
+        <header>
+          <span 
+          className="category">
+            Categoria:<span>{title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard 
+                key ={movie.imdbID} 
+                title={movie.Title} 
+                poster={movie.Poster} 
+                runtime={movie.Runtime} 
+                rating={movie.Ratings[0].Value} 
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
